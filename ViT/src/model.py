31dbc10af67dd1ee7df620a054ed1f1bc81c7b7b
mahmoud etman <metman835@gmail.com>
2026-09-25 19:17:33 +0300

    add vit input layer which adding cls token and positional embeddings

--- ViT/src/model.py
+++ ViT/src/model.py
@@ -1,13 +1,14 @@
 import torch
 import torch.nn as nn
 
-class Patchcreation(nn.Module):
+class PatchCreation(nn.Module):
     def __init__(
         self,
         input_channel: int,
         patch_size: int,
         embedding_dim: int
     ) -> None:
+        super(PatchCreation, self).__init__()
         self.patch_size = patch_size
 
         # use conv2d to split the images into patches
@@ -22,6 +23,10 @@ class Patchcreation(nn.Module):
         self.flatten = nn.Flatten(start_dim=2)
 
     def forward(self, x):
+        """
+        we have an image convert it into
+        14 * 14 -> 196 patches which each patch conatains of 16 * 16 * 3 -> 768
+        """
         # x.shape -> [B, C, H, W]
         # last one -> W, H = W
         img_dim = x.shape[-1]
@@ -50,3 +55,71 @@ class Patchcreation(nn.Module):
 
         # Batch × Number_of_tokens × Embedding_dimension
         return x
+
+class ViTInputLayer(nn.Module):
+    def __init__(
+        self,
+        input_channel: int,
+        image_size: int,
+        patch_size: int,
+        embedding_dim: int,
+        dropout_rate: float
+    ) -> None:
+        super(ViTInputLayer, self).__init__()
+        # get the patch embbeding
+        self.patch_embbedings = PatchCreation(
+            input_channel=input_channel,
+            patch_size=patch_size,
+            embedding_dim=embedding_dim
+        )
+
+        # create a cls token which will be learnable
+        # will be of shape [1, 1, 768] to add to patch embedding
+        # CLS = learned representation useful for classification
+        self.cls_token = nn.Parameter(
+            torch.randn(1, 1, embedding_dim),
+            requires_grad=True
+        )
+
+        # get the number of patches
+        # 224 // 16 -> 14 * 14
+        num_patches = (image_size // patch_size) ** 2
+        # add one for cls token
+        num_positions = num_patches + 1
+
+        # create learnable positional embedding
+        # shape of [1, 197, 768]
+        self.positional_embedding = nn.Parameter(
+            torch.randn(1, num_positions, embedding_dim),
+            requires_grad=True
+        )
+
+        self.dropout = nn.Dropout(dropout_rate)
+
+    def forward(self, x):
+        # x shape -> [B, C, H, W]
+        # B
+        batch_size = x.shape[0]
+
+        # get patch embeddings 
+        # input -> [B, C, H, W]
+        # output -> [B, N, D]
+        patch_embeddings = self.patch_embbedings(x)
+
+        # expand the cls token to add batch_size
+        # from [1, 1, 768] to [batch_size, 1, 768]
+        cls_token = self.cls_token.expand(batch_size, -1, -1) # -1 mean to change this dim
+
+        # we'll concatenate cls token with patch emb
+        # [B, N, D]
+        # B = batch
+        # N = number of tokens -> dim = 1
+        # D = embedding dimension
+        # cls_token ->     [8, 1, 768]
+        # patch_dim ->     [8, 196, 768]
+        # output of this-> [8, 197, 768]
+        tokens = torch.concat((cls_token, patch_embeddings), dim=1)
+
+        out = tokens + self.positional_embedding
+        # output shape [B, N+1, D]
+        return self.dropout(out)
